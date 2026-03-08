47de1506e6e46361b13ec5355e13e5fc5d848a36
mohammed taha <mdtahatahaabuoseif@gmail.com>
2026-03-08 06:31:54 +0300

    creating new porfolio using typed js library

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text" , {
+    strings:["react devoloper", ".Net devoloper" , "full stack devolopter"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
